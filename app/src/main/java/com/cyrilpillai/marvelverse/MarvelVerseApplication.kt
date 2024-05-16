e3bbced2fa4bc7e18518ffba3383fbbcae87815a
cyrilpillai <cyrilpillai@gmail.com>
2024-05-16 23:31:19 +0530

    Add KSP & hilt dependencies

---
+++ app/src/main/java/com/cyrilpillai/marvelverse/MarvelVerseApplication.kt
@@ -0,0 +1,8 @@
+package com.cyrilpillai.marvelverse
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MarvelVerseApplication : Application() {
+}
